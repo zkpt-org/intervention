a39f2730fb19042fbc6d3d1bfaa8f3beaa14da75
Leonard Witzel <witzel@post.harvard.edu>
2013-04-25 17:53:37 -0400

    initial commit

---
+++ main.R
@@ -0,0 +1,10 @@
+#!/usr/bin/Rscript
+
+# Include config file.
+source("config.R")
+
+#Run all initializing queries to the database
+# source("database.R")
+
+#Run all initialization sequencs
+source("initialize.R")
