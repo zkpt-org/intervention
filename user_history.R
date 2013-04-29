db741df6d24d664d21d5b8cf899656b1f00d5166
Leonard Witzel <witzel@post.harvard.edu>
2013-04-29 18:51:36 -0400

    user history routine cleanup.

--- user_history.R
+++ user_history.R
@@ -1,16 +1,14 @@
 # Goal: User Vector = [between day x-1 to x-n count number of days where activity exists]
 UserHistory<-function(user,start){
-    Sys.setenv(TZ='GMT')
     
-    zeroday=start
-    #zeroday=as.numeric(sqldf(paste0("SELECT MIN(date)+", MONTH+DAY, " from login")))
-    day0 = zeroday - (zeroday %% DAY)
-    print(as.POSIXlt(day0, origin="1970-01-01 00:00.00 UTC"))
-
+    Sys.setenv(TZ='GMT')    
+    day0 = start - (start %% DAY)
+    #print(as.POSIXlt(day0, origin="1970-01-01 00:00.00 UTC"))
+    
     pedometer_init=as.numeric(sqldf(paste0("SELECT MIN(date) FROM pedometer WHERE UserID=",user)))
     login_init=as.numeric(sqldf(paste0("SELECT MIN(date) FROM login WHERE UserID=",user)))
     userinit=min(pedometer_init,login_init)-(min(pedometer_init,login_init) %% DAY)
-    print(as.POSIXlt(userinit, origin="1970-01-01 00:00.00 UTC"))
+    #print(as.POSIXlt(userinit, origin="1970-01-01 00:00.00 UTC"))
     rm(pedometer_init,login_init)
 
     c_dates<-c()
@@ -49,13 +47,8 @@ UserHistory<-function(user,start){
             c_bp[i]=length(bp[bp$UserId==user & bp$date >= d-DAY & bp$date < d,1])
             c_weight[i]=length(weight[weight$UserId==user & weight$date >= d-DAY & weight$date < d,1])
             c_glucose[i]=length(glucose[glucose$UserId==user & glucose$date >= d-DAY & glucose$date < d,1])
-            c_weekend[i]=length(login[login$UserId==user & login$date >= d-DAY & login$date < d & (login$dayofweek==0 | login$dayofweek==6),1])
-            # c_login[i]=as.numeric(sqldf(paste0("SELECT COUNT(UserId) FROM login WHERE UserId=", user, " AND date >=", d-DAY, " AND date <", d)))
-            # c_pedometer[i]=as.numeric(sqldf(paste0("SELECT COUNT(UserId) FROM pedometer WHERE UserId=", user, " AND date >=", d-DAY, " AND date <", d)))
-            # c_bp[i]=as.numeric(sqldf(paste0("SELECT COUNT(UserId) FROM bp WHERE UserId=", user, " AND date >=", d-DAY, " AND date <", d)))
-            # c_weight[i]=as.numeric(sqldf(paste0("SELECT COUNT(UserId) FROM weight WHERE UserId=", user, " AND date >=", d-DAY, " AND date <", d)))
-            # c_glucose[i]=as.numeric(sqldf(paste0("SELECT COUNT(UserId) FROM glucose WHERE UserId=", user, " AND date >=", d-DAY, " AND date <", d)))
-            # c_weekend[i]=as.numeric(sqldf(paste0("SELECT COUNT(UserId) FROM login WHERE UserId=", user, " AND date >=", d-DAY, " AND date <", d, " AND (dayofweek = 0 OR dayofweek = 6)")))
+            c_weekend[i]=length(login[login$UserId==user & login$date >= d-DAY & login$date < d & 
+                                     (login$dayofweek==0 | login$dayofweek==6),1])
         }
         c_selftracking[i]=c_weight+c_bp+c_glucose
     }
@@ -71,6 +64,7 @@ UserHistory<-function(user,start){
     return(user_history)
     # rm(c_bp,c_glucose,c_login,c_pedometer,c_selftracking,c_weekend,c_weight,c_dates)
 }
+
 start=as.numeric(sqldf("SELECT MAX(date) from login"))
 user=8099
 user_history<-UserHistory(user,start)
