43e6250f18d0bb0da63aae08b42c38bc21f07acb
Leonard Witzel <witzel@post.harvard.edu>
2013-04-30 15:23:43 -0400

    Outer main loop added for generating activity summary.

--- main.R
+++ main.R
@@ -11,3 +11,29 @@ source("initialize.R")
 
 # Consrtruct user history
 source("user_history.R")
+
+
+start=as.numeric(sqldf("SELECT MAX(date) from login"))
+duration=28
+
+# MAX UserId is 19441
+user_min=8099
+user_max=8110
+users=sqldf(paste0("SELECT DISTINCT UserId FROM login WHERE UserId >=",user_min," AND UserId<=",user_max))
+
+history<-data.frame("user"=users$UserId,"login"=NA,"pedometer"=NA,"bp"=NA,"glucose"=NA,
+                     "weight"=NA,"weekend"=NA,"questions"=NA,"tracking"=NA)
+i=1
+apply(history["user"],1,function(user){
+    user_history<-UserHistory(user,start,duration)
+    history$pedometer[i] <<- length(user_history$pedometer[user_history$pedometer>0])
+    history$login[i]     <<- length(user_history$login[user_history$login>0])
+    history$bp[i]        <<- length(user_history$bp[user_history$bp>0])
+    history$glucose[i]   <<- length(user_history$glucose[user_history$glucose>0])
+    history$weight[i]    <<- length(user_history$weight[user_history$weight>0])
+    history$weekend[i]   <<- length(user_history$weekend[user_history$weekend>0])
+    history$questions[i] <<- length(user_history$questions[user_history$questions>0])
+    history$tracking[i]  <<- length(user_history$tracking[user_history$tracking>0])
+    
+    i<<-i+1
+})
--- user_history.R
+++ user_history.R
@@ -1,13 +1,25 @@
 # Goal: User Vector = [between day x-1 to x-n count number of days where activity exists]
-UserHistory<-function(user,start){
-    
+
+UserHistory<-function(user,start,duration){
+    user=as.numeric(user)
+
+    print(user)
     Sys.setenv(TZ='GMT')    
     day0 = start - (start %% DAY)
     #print(as.POSIXlt(day0, origin="1970-01-01 00:00.00 UTC"))
+
+    if(!is.na(as.numeric(sqldf(paste0("SELECT initdate FROM pedometer WHERE UserId=",user," ORDER BY initdate ASC LIMIT 1")))))
+        pedometer_init=as.numeric(sqldf(paste0("SELECT initdate FROM pedometer WHERE UserId=",user," ORDER BY initdate ASC LIMIT 1")))
+    else
+        pedometer_init=as.numeric(sqldf("SELECT MAX(date) FROM pedometer"))
     
-    pedometer_init=as.numeric(sqldf(paste0("SELECT MIN(date) FROM pedometer WHERE UserID=",user)))
-    login_init=as.numeric(sqldf(paste0("SELECT MIN(date) FROM login WHERE UserID=",user)))
-    userinit=min(pedometer_init,login_init)-(min(pedometer_init,login_init) %% DAY)
+    if(!is.na(as.numeric(sqldf(paste0("SELECT initdate FROM login WHERE UserId=",user," ORDER BY initdate ASC LIMIT 1")))))
+        login_init=as.numeric(sqldf(paste0("SELECT initdate FROM login WHERE UserId=",user," ORDER BY initdate ASC LIMIT 1")))
+    else
+        login_init=as.numeric(sqldf("SELECT MAX(date) FROM login"))
+
+    minim=min(pedometer_init,login_init)
+    userinit=minim-(minim %% DAY)
     #print(as.POSIXlt(userinit, origin="1970-01-01 00:00.00 UTC"))
     rm(pedometer_init,login_init)
 
@@ -18,28 +30,31 @@ UserHistory<-function(user,start){
     c_weight<-c()
     c_glucose<-c()
     c_weekend<-c()
+    c_questions<-c()
     c_selftracking<-c()
-    length(c_dates)<-30
-    length(c_login)<-30
-    length(c_pedometer)<-30
-    length(c_bp)<-30
-    length(c_weight)<-30
-    length(c_glucose)<-30
-    length(c_weekend)<-30
-    length(c_selftracking)<-30
+    length(c_dates)<-duration
+    length(c_login)<-duration
+    length(c_pedometer)<-duration
+    length(c_bp)<-duration
+    length(c_weight)<-duration
+    length(c_glucose)<-duration
+    length(c_weekend)<-duration
+    length(c_questions)<-duration
+    length(c_selftracking)<-duration
 
-    for(i in 1:30){
+    for(i in 1:duration){
         d = day0 - (i*DAY) + DAY
         c_dates[i]=d
         #print(as.POSIXlt(d, origin="1970-01-01 00:00.00 UTC"))
-    
         if(d<userinit){
             c_login[i]<-NA
             c_pedometer[i]<-NA
             c_bp[i]<-NA
             c_weight[i]<-NA
             c_glucose[i]<-NA
-            c_weekend[i]<-NA       
+            c_weekend[i]<-NA
+            c_questions[i]<-NA
+            c_selftracking[i]<-NA
         }
         else{
             c_login[i]=length(login[login$UserId==user & login$date >= d-DAY & login$date < d,1])
@@ -49,22 +64,20 @@ UserHistory<-function(user,start){
             c_glucose[i]=length(glucose[glucose$UserId==user & glucose$date >= d-DAY & glucose$date < d,1])
             c_weekend[i]=length(login[login$UserId==user & login$date >= d-DAY & login$date < d & 
                                      (login$dayofweek==0 | login$dayofweek==6),1])
-        }
-        c_selftracking[i]=c_weight+c_bp+c_glucose
+            c_questions[i]=length(questions[questions$UserId==user & questions$date >= d-DAY & questions$date < d,1])
+            c_selftracking[i]=c_weight+c_bp+c_glucose
+        }       
     }
-    user_history<-merge(data.frame("day"=1:30, c_dates), data.frame("day"=1:30, c_login), by="day")
-    user_history<-merge(user_history, data.frame("day"=1:30, c_pedometer), by="day")
-    user_history<-merge(user_history, data.frame("day"=1:30, c_bp), by="day")
-    user_history<-merge(user_history, data.frame("day"=1:30, c_weight), by="day")
-    user_history<-merge(user_history, data.frame("day"=1:30, c_glucose), by="day")
-    user_history<-merge(user_history, data.frame("day"=1:30, c_weekend), by="day")
-    user_history<-merge(user_history, data.frame("day"=1:30, c_selftracking), by="day")
+    user_history<-merge(data.frame("day"=1:duration, c_dates), data.frame("day"=1:duration, c_login), by="day")
+    user_history<-merge(user_history, data.frame("day"=1:duration, c_pedometer), by="day")
+    user_history<-merge(user_history, data.frame("day"=1:duration, c_bp), by="day")
+    user_history<-merge(user_history, data.frame("day"=1:duration, c_weight), by="day")
+    user_history<-merge(user_history, data.frame("day"=1:duration, c_glucose), by="day")
+    user_history<-merge(user_history, data.frame("day"=1:duration, c_weekend), by="day")
+    user_history<-merge(user_history, data.frame("day"=1:duration, c_questions), by="day")
+    user_history<-merge(user_history, data.frame("day"=1:duration, c_selftracking), by="day")
     colnames(user_history)=c("day","dates","login","pedometer","bp","weight","glucose","weekend","tracking")
     
     return(user_history)
     # rm(c_bp,c_glucose,c_login,c_pedometer,c_selftracking,c_weekend,c_weight,c_dates)
 }
-
-start=as.numeric(sqldf("SELECT MAX(date) from login"))
-user=8099
-user_history<-UserHistory(user,start)
